47c94a0ddab2a21a91e24de1a25c0df1b32e55df
evizitei <ethan.vizitei@gmail.com>
2010-05-15 09:43:37 -0500

    fix cucumber tests to get them all passing

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,11 +1,16 @@
+Given /^there is a firefighter named "([^\"]*)"$/ do |name|
+  Factory(:normal_user,:name=>name,:password => "fire_district",:password_confirmation => "fire_district")
+end
+
 Given /^I am logged in as a firefighter named "([^\"]*)"$/ do |name|
-  user = Factory(:normal_user,:name=>name,:password => "fire_district",:password_confirmation => "fire_district")
+  Given %Q{there is a firefighter named "#{name}"}
+  user = User.find_by_name(name)
   Given %{I sign in as "#{user.email}/fire_district"}
 end
 
-Given /^there is a firefighter named "([^\"]*)" who is subscribed to messages for "([^\"]*)"$/ do |user_name, station_name|
-  Given %Q{there is a firefighter named "#{user_name}"}
-  @notifiee = User.find_by_name
+Given /^there is a firefighter named "([^\"]*)" who is subscribed to messages for "([^\"]*)"$/ do |name, station_name|
+  Given %Q{there is a firefighter named "#{name}"}
+  @notifiee = User.find_by_name(name)
   station = Station.find_by_name(station_name)
   Factory(:subscription,:user=>@notifiee,:station=>station)
 end
