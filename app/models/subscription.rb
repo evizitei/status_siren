bdda44eba67efdd29b9adb695a4bd5b088834e1e
evizitei <ethan.vizitei@gmail.com>
2010-05-15 14:32:48 -0500

    new subscription scope for finding by station object
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,6 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :station
+  
+  named_scope :for_station,lambda{|station| {:conditions=>{:station_id=>station.id}}}
 end
--- test/unit/subscription_test.rb
+++ test/unit/subscription_test.rb
@@ -3,4 +3,10 @@ require 'test_helper'
 class SubscriptionTest < ActiveSupport::TestCase
   should_belong_to :station
   should_belong_to :user
+  
+  should "have scope for station id" do
+    station = Factory(:station)
+    subscription = Factory(:subscription,:station=>station)
+    assert Subscription.for_station(station).any?{|sub| sub.station == station}
+  end
 end
