70bdf82f4bd2594e60b7415721e1081c83152969
evizitei <ethan.vizitei@gmail.com>
2010-05-15 18:06:20 -0500

    new seed that includes stephen

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,13 @@
 bcfpd = Department.find_or_create_by_name(:name=>"Boone County Fire Protection District")
 station = Station.find_or_create_by_name(:name=>"Station 14")
 station.update_attributes!(:department_id=>bcfpd.id)
+
 ethan = User.find_or_create_by_email("ethan.vizitei@gmail.com")
 ethan.update_attributes!(:name=>"Ethan Vizitei",:department_id=>bcfpd.id,:password=>"fire_district",:email=>"ethan.vizitei@gmail.com",:email_confirmed=>true,:mobile_phone=>"5732395840")
 ethan.update_password("fire_district","fire_district")
-Subscription.create!(:station=>station,:user=>ethan)
+
+Subscription.find_or_create_by_station_id_and_user_id(station.id,:user=>ethan.id)
+
+stephen = User.find_or_create_by_email("sdunkin@bcfdmo.com")
+stephen.update_attributes!(:name=>"Stephen Dunkin",:department_id=>bcfpd.id,:password=>"fire_district",:email=>"ethan.vizitei@gmail.com",:email_confirmed=>true,:mobile_phone=>"5732390700")
+ethan.update_password("fire_district","fire_district")
